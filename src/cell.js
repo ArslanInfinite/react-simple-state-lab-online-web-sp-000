2ca94433feb5503d38c2ea5f50610648c0399269
ArslanInfinite <ali.arslan319@gmail.com>
2021-03-04 03:26:10 -0500

    commit tree cb41bcdd57cd53661be0701d340dcdb041163c8d

--- src/cell.js
+++ src/cell.js
@@ -1,26 +1,11 @@
-import React, { Component } from 'react';
+import { useState } from "react";
 
-class Cell extends React.Component {
-  constructor(props) {
-    super()
-    this.state = {
-      color: props.value
-    }
-  }
+export const Cell = ({ value }) => {
+  const [color, setColor] = useState(value);
 
-  handleClick = () => {
-    this.setState({
-      color: '#333'
-    })
-  }
-
-  render() {
-    return (
-      <div className="cell" style={{ backgroundColor: this.state.color }} onClick={this.handleClick}>
-      </div>
-    )
-  }
-
-}
-
-export default Cell
+  return <div
+    className="cell"
+    style={{ backgroundColor: color }}
+    onClick={() => setColor("#333")}
+  />;
+};
